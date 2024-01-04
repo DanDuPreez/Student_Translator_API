5ceb555e03382864366334d6c781d9cc80a12c65
Daniel du Preez <dandupreez@legiondev.co.za>
2024-01-04 14:04:45 +0200

    Student solution

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
+df = pd.read_csv("dictionary.csv")
+
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    return render_template("home.html")
+
+
+@app.route('/api/v1/<word>')
+def api(word):
+    definition = df.loc[df['word'] == word]['definition'].squeeze()
+    result_dictionary = {'word': word, 'definition': definition}
+    return result_dictionary
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
